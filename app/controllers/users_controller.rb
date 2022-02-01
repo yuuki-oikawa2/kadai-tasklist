a0e3d237b7d5b9358d82ee757eb33c6029182e07
yuuki.oikawa2 <yuki.oikawa.g@gmail.com>
2022-02-01 14:14:20 +0000

    update tasklist20220201-02

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
     
-  def index
-   
-  end
-
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   
-  resources :users, only: [:index, :new, :create]
+  resources :users
   
   resources :tasks
   
