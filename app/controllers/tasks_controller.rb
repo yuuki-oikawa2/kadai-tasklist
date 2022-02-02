c86dfffc61a6de45be73fb10ffd5fb373f312e42
yuuki.oikawa2 <yuki.oikawa.g@gmail.com>
2022-02-02 08:23:53 +0000

    update tasklist20220202-3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
     
@@ -55,9 +55,7 @@ class TasksController < ApplicationController
   private
 
   # Strong Parameter
-  def set_task
-    @task = Task.find(params[:id])
-  end
+
   
   def task_params
     params.require(:task).permit(:content, :status, :user_id )
