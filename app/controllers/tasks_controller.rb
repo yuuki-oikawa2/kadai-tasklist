63b05c03b9a33f6ec506509ce57db2ec489b94ad
yuuki.oikawa2 <yuki.oikawa.g@gmail.com>
2022-02-02 09:10:24 +0000

    update tasklist-20220202-4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
     
   def index
-      @task = current_user.tasks.build
       @pagy, @task = pagy(current_user.tasks.order(id: :desc))
   end
 
